6f07aa85e68a5b74041f1d623331d9e7d699d963
Simon Stratmann <stratmann.simon@gmail.com>
2023-01-25 17:07:02 +0100

    Can find boxes for names

---
+++ hsvMaskHelper.py
@@ -0,0 +1,86 @@
+# This is a sample Python script.
+import cv2
+import numpy as np
+
+def nothing(x):
+    pass
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    img = cv2.imread('jannikSpeaking1200.png')
+
+
+    cv2.namedWindow('image')
+
+
+
+    # create trackbars for color change
+    cv2.createTrackbar('HMin', 'image', 0, 179, nothing)  # Hue is from 0-179 for Opencv
+    cv2.createTrackbar('SMin', 'image', 0, 255, nothing)
+    cv2.createTrackbar('VMin', 'image', 0, 255, nothing)
+    cv2.createTrackbar('HMax', 'image', 0, 179, nothing)
+    cv2.createTrackbar('SMax', 'image', 0, 255, nothing)
+    cv2.createTrackbar('VMax', 'image', 0, 255, nothing)
+
+    # Set default value for MAX HSV trackbars.
+    cv2.setTrackbarPos('HMax', 'image', 179)
+    cv2.setTrackbarPos('SMax', 'image', 255)
+    cv2.setTrackbarPos('VMax', 'image', 255)
+
+    # Initialize to check if HSV min/max value changes
+    hMin = sMin = vMin = hMax = sMax = vMax = 0
+    phMin = psMin = pvMin = phMax = psMax = pvMax = 0
+
+    output = img
+    waitTime = 33
+
+    while (1):
+
+        # get current positions of all trackbars
+        hMin = cv2.getTrackbarPos('HMin', 'image')
+        sMin = cv2.getTrackbarPos('SMin', 'image')
+        vMin = cv2.getTrackbarPos('VMin', 'image')
+
+        hMax = cv2.getTrackbarPos('HMax', 'image')
+        sMax = cv2.getTrackbarPos('SMax', 'image')
+        vMax = cv2.getTrackbarPos('VMax', 'image')
+
+        # Set minimum and max HSV values to display
+        lower = np.array([hMin, sMin, vMin])
+        upper = np.array([hMax, sMax, vMax])
+
+        # Create HSV Image and threshold into a range.
+        hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, lower, upper)
+        output = cv2.bitwise_and(img, img, mask=mask)
+
+        # Print if there is a change in HSV value
+        if ((phMin != hMin) | (psMin != sMin) | (pvMin != vMin) | (phMax != hMax) | (psMax != sMax) | (pvMax != vMax)):
+            print("(hMin = %d , sMin = %d, vMin = %d), (hMax = %d , sMax = %d, vMax = %d)" % (
+            hMin, sMin, vMin, hMax, sMax, vMax))
+            phMin = hMin
+            psMin = sMin
+            pvMin = vMin
+            phMax = hMax
+            psMax = sMax
+            pvMax = vMax
+
+        # Display output image
+        cv2.imshow('image', output)
+
+        # Wait longer to prevent freeze for videos.
+        if cv2.waitKey(waitTime) & 0xFF == ord('q'):
+            break
+
+    # cv2.waitKey(0)
+
+    cv2.destroyAllWindows()
+
+    # assert img is not None, "file could not be read, check with os.path.exists()"
+    # imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # ret, thresh = cv2.threshold(imgray, 127, 255, 0)
+    # im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    # cnt = contours[4]
+    # cv2.drawContours(img, [cnt], 0, (0,255,0), 3)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- speakerName.py
+++ speakerName.py
@@ -21,14 +21,48 @@ def areaFilter(minArea, inputImage):
     return filteredImage
 
 
+def findSpeakerNameCoordinates():
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    _, bw_copy = cv2.threshold(gray, 0.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+    # bilateral filter
+    blur = cv2.bilateralFilter(gray, 5, 75, 75)
+
+    # morphological gradient calculation
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    grad = cv2.morphologyEx(blur, cv2.MORPH_GRADIENT, kernel)
+
+    # binarization
+    _, bw = cv2.threshold(grad, 0.0, 255.0, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+    # closing
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 1))
+    closed = cv2.morphologyEx(bw, cv2.MORPH_CLOSE, kernel)
+    # cv2.imshow('closed', closed)
+    candidateContours = []
+    contours, hierarchy = cv2.findContours(closed, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    nameBoxCoordinates = []
+
+    for c in contours:
+        x, y, w, h = cv2.boundingRect(c)
+        # if 700 < y < 720 and x < 680:
+        if 40 < w < 55 and 7 < h < 20:
+            candidateContours.append(c)
+            nameBoxCoordinates.append((x, y, w, h))
+    image_copy = img.copy()
+    # cv2.drawContours(image_copy, candidateContours, -1, (0, 255, 0), 2)
+    # cv2.imshow('candidateContours', image_copy)
+    return nameBoxCoordinates
+
 def findSpeakingIndicatorCoordinates():
     image_copy = img.copy()
     hsvImage = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     # Create the HSV mask
-    lowerValues = np.array([96, 65, 210])
-    upperValues = np.array([121, 125, 247])
+    lowerValues = np.array([15, 118, 211])
+    upperValues = np.array([21, 186, 255])
     hsvMask = cv2.inRange(hsvImage, lowerValues, upperValues)
-    # cv2.imshow("hsvMask", hsvMask)
+    cv2.imshow("hsvMask", hsvMask)
     # cv2.imshow("Image", hsvImage)
     minArea = 50
     cleanedMask = areaFilter(minArea, hsvMask)
@@ -59,31 +93,46 @@ def findSpeakingIndicatorCoordinates():
             rectHeight = boundRect[3]
             indicatorCoordinateTuples.append((rectX, rectY, rectWidth, rectHeight))
     cv2.drawContours(image_copy, indicatorContours, -1, (0, 255, 0), 2)
-    # cv2.imshow("cleanedMask", cleanedMask)
+    cv2.imshow("cleanedMask", cleanedMask)
     cv2.imshow("cleanedMaskContours", image_copy)
     return indicatorCoordinateTuples
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    img = cv2.imread('jannikSpeaking.png')
+    img = cv2.imread('jannikSpeaking1200.png')
 
+    # imgFloat = img.astype("float") / 255.
+    #
+    # # Calculate channel K:
+    # kChannel = 1 - np.max(imgFloat, axis=2)
+    #
+    # # Convert back to uint 8:
+    # kChannel = (255 * kChannel).astype(np.uint8)
+    # cv2.imshow("kChannel", kChannel)
+    # binaryThreshMin = 255 * 0.4
+    # binaryThreshMax = 255 * 0.41
+    # _, binaryImage = cv2.threshold(kChannel, binaryThreshMin, binaryThreshMax, cv2.THRESH_BINARY)
+    #
+    # cv2.imshow("binaryImage", cv2.bitwise_not(binaryImage))
+    # cv2.imshow('Image', img.copy())
+    # img = cv2.resize(img, (None, None), None, 0.4, 0.4)
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # img = np.array(img, dtype='float32')
+    # result = np.where(img == 0., 255, 0)
+    # result = np.array(result, dtype='uint8')
+    # result = cv2.erode(result, kernel=np.ones(shape=(3, 3)), iterations=1)
+    # cv2.imshow('Image', img)
 
-    # The HSV mask values:
+    nameBoxCoordinates = findSpeakerNameCoordinates()
+    image_copy = img.copy()
+    for coordinates in nameBoxCoordinates:
+        cv2.rectangle(image_copy, (coordinates[0], coordinates[1]), (coordinates[0] + coordinates[2], coordinates[1] + coordinates[3]), (0, 255, 0), 2)
 
-    speakingIndicatorCoordinateTuples = findSpeakingIndicatorCoordinates()
-    print(speakingIndicatorCoordinateTuples)
+    cv2.imshow('Name boxes', image_copy)
+
     cv2.waitKey(0)
 
     cv2.destroyAllWindows()
 
-    # https://stackoverflow.com/questions/66510087/how-to-detect-a-precise-rectangle-in-a-photo-opencv
 
-    # assert img is not None, "file could not be read, check with os.path.exists()"
-    # imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # ret, thresh = cv2.threshold(imgray, 127, 255, 0)
-    # im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # cnt = contours[4]
-    # cv2.drawContours(img, [cnt], 0, (0,255,0), 3)
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
